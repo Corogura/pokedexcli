4c64b1c8f3b1920ebb64ae78d56fc0c42944d763
Corogura <shh.psn.cc@gmail.com>
2025-05-23 02:06:43 +0900

    L5

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		var input string
-		scanner.Scan()
-		input = scanner.Text()
-		cleanedInput := cleanInput(input)
-		fmt.Printf("Your command was: %v\n", cleanedInput[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,11 +1,71 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		var input string
+		scanner.Scan()
+		input = scanner.Text()
+		cleanedInput := cleanInput(input)
+		c, exists := commands[cleanedInput[0]]
+		if exists {
+			err := c.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	slicedString := strings.Fields(text)
 	return slicedString
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Print("Usage:\n\n")
+	for _, elem := range getCommands() {
+		fmt.Printf("%v: %v\n", elem.name, elem.description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
